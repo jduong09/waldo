0ced3ba8dcaedc6233d613606eb299b671cef340
Justin Duong <jduong@umich.edu>
2023-07-19 18:26:16 -0400

    Began adding data to data object.

--- assets/mouse.js
+++ assets/mouse.js
@@ -1,3 +1,34 @@
+/*
+Data: 
+Type: object
+{
+  dark_souls_3: {
+    character_1: {
+      locationOffset: [offSetX, offSetY],
+      gameLocation: 'String'
+      type: 'boss' || 'enemy' || 'npc'
+    },
+  },
+  other_img: {
+    character_1: {
+      locationOffset: [offSetX, offSetY]
+    }
+  }
+}
+On App Start
+  - Choose 3 Characters to choose from Image
+    - randomized choosing
+  - Display to user 3 characters
+
+Event Listeners
+  - IMG hover
+    - Show tagging aim
+  - IMG click
+    - Take offSet data and check if user has chosen the correct character
+  - Play Game Click
+    - Remove intro modal?
+*/
+
 document.addEventListener('DOMContentLoaded', () => {
   const headerTurnResult = document.getElementById('header-turn-result');
   const div = document.querySelector('div.div-gallery');
@@ -26,10 +57,13 @@ document.addEventListener('DOMContentLoaded', () => {
     svg.appendChild(rect);
     div.appendChild(svg);
 
-    if (e.offsetX - 25 < 180 && e.offsetX + 25 > 180 && e.offsetY - 37.5 < 170 && e.offsetY + 37.5 > 170) {
+    if (e.offsetX - 25 < darkSoulsData['darkSouls3']['Iudex of Gundyr'].locationOffSet[0][0]
+      && e.offsetX + 25 > darkSoulsData['darkSouls3']['Iudex of Gundyr'].locationOffSet[0][0]
+      && e.offsetY - 37.5 < darkSoulsData['darkSouls3']['Iudex of Gundyr'].locationOffSet[0][1]
+      && e.offsetY + 37.5 > darkSoulsData['darkSouls3']['Iudex of Gundyr'].locationOffSet[0][1]) {
       const delta = Date.now() - start;
       console.log(`It took ${Math.floor(delta / 1000)} seconds!`);
-      headerTurnResult.innerHTML = `Found Waldo in ${counter} tries.`;
+      headerTurnResult.innerHTML = `Found Iudex Of Gundyr in ${counter} tries.`;
       clearInterval(intervalId);
     } else {
       headerTurnResult.innerHTML = `Try Again Bitch.`;
@@ -38,6 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }, 1000);
     }
   });
+
   btnPlay.addEventListener('click', () => {
     start = Date.now();
     
@@ -49,81 +84,261 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+const darkSoulsData = {
+  darkSouls3: {
+    'Iudex of Gundyr': {
+      locationOffSet: [[2203, 2102]],
+      gameLocation: 'Cementary Of Ash',
+      type: 'boss'
+    },
+    'Grave Warden': {
+      locationOffSet: [[2278, 2239], [2189, 2235], [2116, 2219], [2052, 2196]],
+      gameLocation: 'Cementary Of Ash',
+      type: 'enemy'
+    },
+    'Ravenous Crystal Lizard': {
+      locationOffSet: [[2404, 2174]],
+      gameLocation: 'Cementary Of Ash',
+      type: 'enemy'
+    },
+    'Anri of Astora': {
+      locationOffSet: [[1739, 1719]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Blacksmith Andre': {
+      locationOffSet: [[2058, 1988]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Cornyx of the Great Swamp': {
+      locationOffSet: [[1808, 1966]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Eygon of Carim': {
+      locationOffSet: [[1804, 1833]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Fire Keeper': {
+      locationOffSet: [[1918, 1753]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Greirat of the Undead Settlement': {
+      locationOffSet: [[2191, 1710]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Hawkwood': {
+      locationOffSet: [[2160, 1840]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Horace the Hushed': {
+      locationOffSet: [[1823, 1688]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Irina of Carim': {
+      locationOffSet: [[1724, 1841]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Karla': {
+      locationOffSet: [[2051, 1873]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Leonhard the Ringfinger': {
+      locationOffSet: [[2058, 1653]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Ludleth of Courland': {
+      locationOffSet: [[1916, 1649]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Orbeck of Vinheim': {
+      locationOffSet: [[1888, 1961]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Pickle Pee, Pump-a-Rum Crow': {
+      locationOffSet: [[1994, 1642]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Shrine Handmaid ': {
+      locationOffSet: [[1981, 1997]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Sirris of the Sunless Realms': {
+      locationOffSet: [[1936, 1866]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Sword Master': {
+      locationOffSet: [[2261, 1786]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Unbreakable Patches': {
+      locationOffSet: [[2130, 1685]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Yoel of Londor': {
+      locationOffSet: [[2246, 1933]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Yuria of Londor': {
+      locationOffSet: [[2156, 1947]],
+      gameLocation: 'Firelink Shrine',
+      type: 'npc'
+    },
+    'Crystal Lizard': {
+      locationOffSet: [[2875, 1557]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Hollow Soldier': {
+      locationOffSet: [[2518, 1565], [2608, 1552], [2441, 1552], [2707, 1577]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Hollow Assassin': {
+      locationOffSet: [[2364, 1663]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Large Hollow Soldier': {
+      locationOffSet: [[2701, 1572]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Lothric Knight Red': {
+      locationOffSet: [[2512, 1452], [2619, 1447]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Lothric Knight Blue': {
+      locationOffSet: [[2423, 1410]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Lothric Wyvern': {
+      locationOffSet: [[1756, 1328], [1930, 1291]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Mimic': {
+      locationOffSet: [[2369, 1487]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Pus of Man': {
+      locationOffSet: [[2765, 1487]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Starved Hound': {
+      locationOffSet: [[2695, 1666], [3149, 2382]],
+      gameLocation: ['Highwall of Lothric', 'Undead Settlement'],
+      type: 'enemy'
+    },
+    'Winged Knight': {
+      locationOffSet: [[2275, 1556]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'enemy'
+    },
+    'Vordt of the Boreal Valley': {
+      locationOffSet: [[2717, 1322]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'boss'
+    },
+    'Dancer of the Boreal Valley': {
+      locationOffSet: [[2464, 1275]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'boss'
+    },
+    'Emma': {
+      locationOffSet: [[2338, 1382]],
+      gameLocation: 'Highwall of Lothric',
+      type: 'npc'
+    },
+    'Siegward of Catarina': {
+      locationOffSet: [[628, 3133]],
+      gameLocation: 'Undead Settlement',
+      type: 'npc'
+    },
+    'Holy Knight Hodrick': {
+      locationOffSet: [[2898, 1865]],
+      gameLocation: 'Undead Settlement',
+      type: 'npc'
+    },
+    'Hollow Manservant with Cage': {
+      locationOffSet: [[3138, 2002]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+    'Giant Slave': {
+      locationOffSet: [[2834, 1697]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+    'Curse-rotted Greatwood': {
+      locationOffSet: [[3015, 1802]],
+      gameLocation: 'Undead Settlement',
+      type: 'boss'
+    },
+    'Peasant Hollow': {
+      locationOffSet: [[2845, 2166], [2912, 2128], [2739, 2168], [2665, 2133]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+
+
+    'Giant Slave': {
+      locationOffSet: [[2834, 1697]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+    'Giant Slave': {
+      locationOffSet: [[2834, 1697]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+    'Giant Slave': {
+      locationOffSet: [[2834, 1697]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+    'Giant Slave': {
+      locationOffSet: [[2834, 1697]],
+      gameLocation: 'Undead Settlement',
+      type: 'enemy'
+    },
+
+  }
+};
+
 /*
 Default Classes (Optional)
 */
 
 /*
-Cementary of Ash
-_________________
-Iudex of Gundyr [2203, 2102]
-Grave Warden [2278, 2239] [2189, 2235] [2116, 2219] [2052, 2196]
-Ravenous Crystal Lizard [2404, 2174]
-*/
-
-/*
 FireLink Shrine
-___________________
-Anri of Astora [1739, 1719]
-Blacksmith Andre [2058, 1988]
-Cornyx of the Great Swamp [1808, 1966]
-Eygon of Carim [1804, 1833]
-Fire Keeper - [1918, 1753]
-Greirat of the Undead Settlement [2191, 1710]
-Hawkwood [2160, 1840]
-Horace the Hushed [1823, 1688]
-Irina of Carim [1724, 1841]
-Karla [2051, 1873]
-Leonhard the Ringfinger [2058, 1653]
-Ludleth of Courland [1916, 1649]
-Orbeck of Vinheim [1888, 1961]
-Pickle Pee, Pump-a-Rum Crow [1994, 1642]
-Shrine Handmaid [1981, 1997]
-Sirris of the Sunless Realms [1936, 1866]
-Sword Master (hostile) [2261, 1786]
-Unbreakable Patches [2130, 1685]
-Yoel of Londor [2246, 1933]
-Yuria of Londor [2156, 1947]
-*/
-
-/*
 Highwall of Lothric
-____________________
-
-Crystal Lyzard [2875, 1557]
-Darkwraith
-Hollow Soldier Sword and Shield [2518, 1565]
-Hollow Soldier Crossbow [2608, 1552]
-Hollow Soldier Spear and Shield [2441, 1552]
-Hollow Soldier GreatAxe [2707, 1577]
-Hollow Assassin [2364, 1663]
-Large Hollow Soldier
-Lothric Knight Red [2512, 1452] [2619, 1447]
-Lothric Knight Blue [2423, 1410]
-Lothric Wyvern [1756, 1328] [1930, 1291]
-Mimic [2369, 1487]
-Pus of Man [2765, 1487]
-Starved hound [2695, 1666]
-Winged Knight [2275, 1556]
-Vordt of the Boreal Valley [2717, 1322]
-Dancer of the Boreal Valley [2464, 1275]
-Emma (NPC) [2338, 1382]
 */
 
 /*
 Undead Settlement
 __________________
-Siegward of Catarina [628, 3133]
-Velka the Goddess of Sin
-Holy Knight Hodrick [2898, 1865]
-Mound-Maker Covenant
-Warrior of Sunlight Covenant
-Hollow Manservant with Cage. Transports player to Mound-Maker Covenant. [3138, 2002]
-Giant Slave [2834, 1697]
-Curse-rotted Greatwood [3015, 1802]
-Hollow
-Starved Hound [3149, 2382]
-Peasant Hollow [2845, 2166] [2912, 2128] [2739, 2168] [2665, 2133]
 Hollow Slave [2688, 2036]
 Cage Spider [2609, 1993]
 Cathedral Evangelist [2782, 2068]
