0ba3306e61c12a03d8727e631431a5b184aab3eb
Justin Duong <jduong@umich.edu>
2023-07-18 20:56:40 -0400

    added data for the rest of base game and Ashes of Ariandel DLC.

--- assets/mouse.js
+++ assets/mouse.js
@@ -47,12 +47,23 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(Math.floor(delta / 1000));
     }, 1000);
   });
-})
+});
+
+/*
+Default Classes (Optional)
+*/
+
+/*
+Cementary of Ash
+_________________
+Iudex of Gundyr [2203, 2102]
+Grave Warden [2278, 2239] [2189, 2235] [2116, 2219] [2052, 2196]
+Ravenous Crystal Lizard [2404, 2174]
+*/
 
 /*
 FireLink Shrine
 ___________________
-
 Anri of Astora [1739, 1719]
 Blacksmith Andre [2058, 1988]
 Cornyx of the Great Swamp [1808, 1966]
@@ -73,7 +84,6 @@ Sword Master (hostile) [2261, 1786]
 Unbreakable Patches [2130, 1685]
 Yoel of Londor [2246, 1933]
 Yuria of Londor [2156, 1947]
-
 */
 
 /*
@@ -161,4 +171,171 @@ Giant Slave
 Ravenous Crystal Lizard
 Deep Accursed [2921, 2940]
 Longfinger Kirk (NPC invader) [3292, 3012]s
+*/
+
+/*
+Farron Keep
+_____________
+Abyss Watchers [2037, 2615]
+Stray Demon [2172, 2649]
+Darkwraith [1649, 2753]
+Basilisk [2079, 2753]
+Corvian
+Corvian Storyteller
+Crystal Lizard
+Elder Ghru [1938, 2739]
+Ghru [2202, 2817] [2119, 2828] [1849, 2758] [1752, 2760] [1715, 2844]
+Ravenous Crystal Lizard
+Rotten Slug [1801, 2877]
+London Pale Shade [1736, 2641]
+Black Hand Gotthard [1846, 2623]
+*/
+
+/*
+Catacombs of Carthus
+_____________________
+High Lord Wolnir [1358, 2974]
+Skeleton [1449, 3194]
+Skeleton Wheel [1620, 3176]
+Dark Spirit Knight Slayer Tsorig (Invasion) [1644, 3083]
+Carthus Swordsman Skeleton [1335, 3181]
+Grave Warden Skeleton [1142, 3124] [1232, 3165] [1180, 3042] [1301, 3107]
+*/
+
+/*
+Smouldering Lake
+__________________
+Great Swamp Cuculus [2557, 3227]
+Old Demon King [1965, 3051]
+Carthus Sandworm [2133, 3099]
+Demon Cleric [1916, 3250]
+Demon Statue [2004, 3208]
+Great Crab (Ember) [2444, 3140]
+Smoldering Ghru [2477, 3283] [2375, 3379] [2266, 3363] [2171, 3368] [2040, 3340]
+Smoldering Rotten Flesh [1927, 3362]
+*/
+
+/*
+Irithyll Of The Boreal Valley
+_______________________________
+Pontiff Sulyvahn [1132, 2425]
+Burning Stake Witch [910, 2512]
+Irithyllian Slave [976, 2591] [1063, 2638] [1002, 2719] [906, 2702]
+Pontiff Knight (Hag) [835, 2654] [1107, 2737] [1210, 2731]
+Sewer Centipede [804, 2581]
+Sulyvahn's Beast [1456, 2588]
+Irithyllian Beast-hound [1300, 2508]
+Creighton the Wanderer [1391, 2497]
+*/
+
+/* 
+Anor Londo 
+___________
+Company Captain Yorshka [1439, 2114]
+Drang Knights [1211, 2261] [1123, 2256]
+Aldrich, Devourer of Gods [1253, 2028]
+Silver Knight [1316, 2252] [1404, 2242] [1085, 2158]
+*/
+
+/*
+Irithyll Dungeon
+___________________
+Jailer [846, 2948] [751, 2938] [995, 2856] [771, 2828] [610, 2862]
+Alva, Seeker of the Spurned (Invasion) [936, 2962]
+Monstrosity of Sin [363, 3120]
+Wretch [664, 2928] [903, 2875] [1032, 2942]
+*/
+
+/*
+Profaned Capital
+_________________
+Yhorm The Giant [517, 2960]
+Jailer Handmaid [265, 3201]
+Court Sorcerer [747, 3177]
+Headless Gargoyle [574, 3269] [427, 3229]
+*/
+
+/*
+Consumed King's Garden
+________________________
+Oceiros, the Consumed King [1457, 1486]
+Cathedral Knight [1615, 1619] [1413, 1613]
+*/
+
+/* 
+Unintended Grave
+_________________
+Champion Gundyr [1387, 1827]
+Black Knight [1468, 1784] [1282, 1776]
+*/
+
+/*
+Archdragon Peak
+_________________
+Ancient Wyvern [1172, 702]
+Nameless King [1220, 876]
+Serpent-man [1122, 1010] [1081, 1059] [850, 1078]
+Serpent-Man Summoner [717, 919]
+Drakeblood Knight [692, 1043]
+Rock Lizard [1248, 1025]
+Havel Knight [989, 861]
+Rapier Champion [594, 1013]
+*/
+
+/*
+Lothric Castle
+________________
+Pilgrim Butterfly [1748, 961]
+Dragonslayer Armour [1899, 1051]
+Lothric, Younger Prince [2469, 813]
+Lorian, Elder Prince [2341, 793]
+Hollow Assassin [1862, 1407]
+Hollow Priest [1771, 1406]
+Lothric Knight Red [1654, 1382]
+Winged Knight [1981, 1381]
+Boreal Outrider Knight
+Lothric Wyvern [1754, 1293] [1925, 1293]
+*/
+
+/*
+Grand of Archives
+__________________
+Black Hand Trio
+Lion Knight Ardbert [2408, 993]
+Black Hand Kamui [2506, 950]
+Daughter of Crystal Kriemhild [2318, 947]
+Lothric Knight Blue [2468, 1107] [2353, 1100]
+Ascended Winged Knight [2143, 909] [2660, 909]
+Grand Archives Scholar [2264, 1064]
+Clawed Curse [2579, 1070]
+Gargoyle [2643, 1011] [2179, 1006]
+*/
+
+/*
+Kiln of the First Flame
+_________________________
+Soul Of Cinder [2137, 394]
+*/
+
+/* Ashes of Ariandel DLC */
+/*
+Painted World of Ariandel
+____________________________
+Sister Freide [550, 1350]
+Father Ariandel [719, 1306]
+Champion's Gravetender [547, 2057]
+Gravetender Greatwolf [803, 2027]
+Sir Vilhelm [425, 1414]
+Livid Pyromancer Dunnel [255, 1623]
+Corvian Settler [391, 1624]
+Painting Woman [328, 1372]
+Millwood Knight [1047, 1559] [930, 1598] [828, 1516]
+Millwood Chieftain [955, 1456]
+Farron Follower [762, 1710] [572, 1723] [489, 1694]
+Tree Woman [557, 1580] [448, 1936]
+Corvian Settler (Enemy) [888, 1686] [248, 1458]
+Corvian Knight [370, 1520]
+Giant Fly [666, 1622]
+Wolf [827, 1585]
+Greater Crab (Ice) [335, 2064]
 */
